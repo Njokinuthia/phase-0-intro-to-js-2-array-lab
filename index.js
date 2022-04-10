8e252f1435bceb2e7e0c27ff3f19e160b7036005
Njoki <kinuthiash@gmail.com>
2022-04-10 18:04:49 +0300

    add cat name to end of array

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const cats = ["Milo", "Otis", "Garfield"];
+
+let beforeEach = function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+};
+
+function destructivelyAppendCat(name){
+  beforeEach();
+  return cats.push("Ralph")
+}
+
