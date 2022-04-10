b3f19c52ee00963e35d09bb17016d9714e5cf073
Njoki <kinuthiash@gmail.com>
2022-04-10 18:08:04 +0300

    add name to start of array

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// declare original array
 const cats = ["Milo", "Otis", "Garfield"];
 
+// function to reset array
 let beforeEach = function () {
   cats.length = 0;
 
   cats.push("Milo", "Otis", "Garfield");
 };
 
+// add name to end of array
 function destructivelyAppendCat(name){
   beforeEach();
   return cats.push("Ralph")
 }
 
+// add name to start of arrays
+function destructivelyPrependCat(name){
+  beforeEach();
+  return cats.unshift("Bob")
+}
